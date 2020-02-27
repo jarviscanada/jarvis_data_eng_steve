aa9ec0fffc61ee54bfdcdcb13ccf0363e5c232f5
wxlxy95 <liuxinyu9090@gmail.com>
2020-02-27 16:43:03 -0500

    implemented psql_docker.sh

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,10 +1,48 @@
 #! /bin/bash
 
-systemctl status docker || systemctl start docker
+op=$1
+pwd=$2
 
-if [ $(docker ps -f name=jrvs-psql | wc -l) -eq 2 ]; then
-    echo 'jrvs-psql container is running'
-    exit 0
+if [ "$#" -gt 2 ]; then
+    echo "warning: only 2 args are needed, the rest will be ignored"
+fi
+
+if [ "$op" = "stop" ]; then
+    docker stop jrvs-psql
+elif [ "$op" = "start" ]; then
+    if [ -z "$pwd" ]; then
+        echo "error: please provide a postgres password"
+        exit 1
+    fi
+    # systemctl status docker || systemctl start docker 
+    if [ -z "$(systemctl status docker 2> /dev/null)" ]; then
+        echo "starting docker daemon..."
+        systemctl start docker
+    fi
+
+    if [ $(docker ps -f name=jrvs-psql | wc -l) -eq 2 ]; then
+        echo "jrvs-psql container is running already"
+    else
+        # docker inspect pgdata || docker volume create pgdata
+        if [ -z "$(docker inspect --format '{{ .Mountpoint }}' pgdata 2> /dev/null)" ]; then
+            echo "creating local volumn..." 
+            docker volume create pgdata
+        fi
+        # check if `jrvs-psql` container is created
+        if [ $(docker ps -a -f name=jrvs-psql | wc -l) -ne 2 ]; then
+            echo "creating container..."
+            docker create \
+            --name jrvs-psql \
+            -e POSTGRES_PASSWORD=$pwd \
+            -v pgdata:/var/lib/postgresql/data \
+            -p 5432:5432 \
+            postgres
+        fi
+        docker container start jrvs-psql 
+    fi
+else
+    echo "error: '${op}' is not a valid command"
+    exit 1
 fi
 
 exit 0
