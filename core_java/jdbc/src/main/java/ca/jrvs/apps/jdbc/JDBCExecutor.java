1c3a6de8b71b2653820b00cbaed316a9a0efd291
wxlxy95 <liuxinyu9090@gmail.com>
2020-03-31 16:27:13 -0400

    finish lynda 2-3

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -23,6 +23,19 @@ public class JDBCExecutor {
       while (resultSet.next()) {
         logger.info(String.valueOf(resultSet.getInt(1)));
       }
+
+      CustomerDAO customerDAO = new CustomerDAO(connection);
+      Customer customer = new Customer();
+      customer.setFirstName("George");
+      customer.setLastName("Washington");
+      customer.setEmail("george.washington@wh.gov");
+      customer.setPhone("(555) 555-6543");
+      customer.setAddress("1234 Main St");
+      customer.setCity("Mount Vernon");
+      customer.setState("VA");
+      customer.setZipCode("22121");
+
+      customerDAO.create(customer);
     } catch (SQLException e) {
       logger.error(e.getMessage(), e);
     }
