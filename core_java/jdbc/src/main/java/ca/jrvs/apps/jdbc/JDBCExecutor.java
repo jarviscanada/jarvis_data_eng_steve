de3e8e9d310f9b405adce2a85b5e0b14847d1e85
Steven Liu <liuxinyu9090@gmail.com>
2020-04-02 23:51:53 -0400

    Feature/jdbc (#20)
    * finish lynda 2-1
    
    * finish lynda 2-3
    
    * finish lynda 2-5; implement Read
    
    * finish lynda 2-6; implement Update
    
    * finish lynda 2-7; implement Delete
    
    * finish lynda 2-10
    
    * finish lynda 3-1; use stored procedure
    
    * finish lynda 3-3
    
    * apply DRY

---
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -0,0 +1,53 @@
+package ca.jrvs.apps.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JDBCExecutor {
+
+  private static Logger logger = LoggerFactory.getLogger(JDBCExecutor.class);
+
+  public static void main(String[] args) {
+    BasicConfigurator.configure();
+    DatabaseConnectionManager dcm = new DatabaseConnectionManager(
+        "localhost", "hplussport", "postgres", "password");
+    try {
+      logger.info("Order DAO:");
+      Connection connection = dcm.getConnection();
+      OrderDAO orderDAO = new OrderDAO(connection);
+      Order order = orderDAO.findById(1000);
+      logger.info(order.toString());
+
+      logger.info("Using stored procedure:");
+      List<Order> orders = orderDAO.getOrdersForCustomer(789);
+      orders.forEach(x -> logger.info(x.toString()));
+
+      logger.info("Using limit:");
+      CustomerDAO customerDAO = new CustomerDAO(connection);
+      customerDAO.findAllSorted(10).forEach(x -> logger.info(x.toString()));
+
+      logger.info("Paged results:");
+      for (int i = 1; i < 3; i++) {
+        logger.info("Page number: " + i);
+        customerDAO.findAllPaged(10, i).forEach(x -> logger.info(x.toString()));
+      }
+
+      logger.info("Update a record:");
+      Customer customer = customerDAO.findById(10000);
+      customer.setEmail("gwashington@wh.gov");
+      customer = customerDAO.update(customer);
+      logger.info(customer.getFirstName() + " "
+          + customer.getLastName() + " "
+          + customer.getEmail()
+      );
+
+    } catch (SQLException e) {
+      logger.error(e.getMessage(), e);
+    }
+  }
+
+}
