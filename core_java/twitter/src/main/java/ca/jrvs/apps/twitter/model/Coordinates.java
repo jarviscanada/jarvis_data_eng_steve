1157b1ab33f9ffe23db52cf345d614e94157dd25
wxlxy95 <liuxinyu9090@gmail.com>
2020-04-09 15:16:51 -0400

    remove setter; add input checking

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Coordinates.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Coordinates.java
@@ -11,22 +11,30 @@ public class Coordinates {
   @JsonProperty("type")
   private final String type = "Point";
   /**
-   * The longitude and latitude of the Tweet’s location, as a collection in the form [longitude,
-   * latitude].
+   * The longitude and latitude of the Tweet’s location, as a collection in the form {longitude,
+   * latitude}.
    */
   @JsonProperty("coordinates")
   private double[] coordinates;
 
   public Coordinates(double[] coordinates) {
+    if (coordinates.length != 2) {
+      throw new IllegalArgumentException(
+          "Coordinates array must be of length 2: {longitude, latitude}");
+    }
+    if (coordinates[0] < -180 || coordinates[0] > 180) {
+      throw new IllegalArgumentException(
+          "Invalid longitude value, should be in [-180, 180]");
+    }
+    if (coordinates[1] < -90 || coordinates[1] > 90) {
+      throw new IllegalArgumentException(
+          "Invalid latitude value, should be in [-90, 90]");
+    }
     this.coordinates = coordinates;
   }
 
   public double[] getCoordinates() {
     return coordinates;
-  }
-
-  public void setCoordinates(double[] coordinates) {
-    this.coordinates = coordinates;
   }
 
   public String getType() {
