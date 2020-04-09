ae43b36d728f4ef49846607ba7a9f83807749bf3
wxlxy95 <liuxinyu9090@gmail.com>
2020-04-09 19:06:32 -0400

    Coordinates implements GeoCoordinate

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Coordinates.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Coordinates.java
@@ -2,7 +2,10 @@ package ca.jrvs.apps.twitter.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Coordinates {
+public class Coordinates implements GeoCoordinate {
+
+  private static final int LONGITUDE_INDEX = 0;
+  private static final int LATITUDE_INDEX = 1;
 
   /**
    * The type of data encoded in the coordinates property. This will be “Point” for Tweet
@@ -15,14 +18,23 @@ public class Coordinates {
    * latitude}.
    */
   @JsonProperty("coordinates")
-  private double[] coordinates;
+  private double[] coordinatesArray;
 
-  public double[] getCoordinates() {
-    return coordinates;
+  public double[] getCoordinatesArray() {
+    return coordinatesArray;
   }
 
   public String getType() {
     return type;
   }
 
+  @Override
+  public double getLongitude() {
+    return coordinatesArray[LONGITUDE_INDEX];
+  }
+
+  @Override
+  public double getLatitude() {
+    return coordinatesArray[LATITUDE_INDEX];
+  }
 }
---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/GeoCoordinate.java
@@ -0,0 +1,14 @@
+package ca.jrvs.apps.twitter.model;
+
+public interface GeoCoordinate {
+
+  /**
+   * Longitude specifies the east–west position of a point on the Earth's surface
+   */
+  double getLongitude();
+
+  /**
+   * Latitude specifies the north–south position of a point on the Earth's surface.
+   */
+  double getLatitude();
+}
