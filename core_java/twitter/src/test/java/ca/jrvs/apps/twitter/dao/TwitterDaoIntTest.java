51348efb46689abab48d646b45190d94da04dca7
wxlxy95 <liuxinyu9090@gmail.com>
2020-04-14 17:44:34 -0400

    refine style

--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterDaoIntTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterDaoIntTest.java
@@ -1,18 +1,28 @@
 package ca.jrvs.apps.twitter.dao;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import ca.jrvs.apps.twitter.dao.helper.AccessKey;
 import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
 import ca.jrvs.apps.twitter.dao.helper.TwitterHttpHelper;
 import ca.jrvs.apps.twitter.model.Tweet;
 import ca.jrvs.apps.twitter.utils.JsonParser;
 import ca.jrvs.apps.twitter.utils.TweetUtil;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TwitterDaoIntTest {
+
+  private static final String HASHTAG = "#test";
+  private static final String MENTION = "@someone";
+  private static final String TEXT = " Test message from the North Pole.";
+  private static final double LONGITUDE = 135d;
+  private static final double LATITUDE = 90d;
 
   private final Logger logger = LoggerFactory.getLogger(TwitterDaoIntTest.class);
   private TwitterDao dao;
@@ -28,52 +38,41 @@ public class TwitterDaoIntTest {
 
   @Test
   public void createFindDelete() throws Exception {
-    String hashtag = "#test";
-    String mention = "@someone";
     String text =
-        System.currentTimeMillis() + mention + " Test message from the North Pole." + hashtag;
-    double lon = 135;
-    double lat = 90;
-    Tweet tweet = TweetUtil.buildTweet(text, lon, lat);
+        System.currentTimeMillis() + MENTION + TEXT + HASHTAG;
+    Tweet tweet = TweetUtil.buildTweet(text, LONGITUDE, LATITUDE);
     logger.info(JsonParser.toJson(tweet, true, false));
 
     logger.info("Testing create...");
     Tweet postedTweet = dao.create(tweet);
-
-    Assert.assertEquals(text, postedTweet.getText());
-    Assert.assertNotNull(postedTweet.getCoordinates());
-    Assert.assertEquals(2, postedTweet.getCoordinates().getCoordinatesArray().length);
-    Assert.assertEquals(lon, postedTweet.getCoordinates().getLongitude(), 0.1);
-    Assert.assertEquals(lat, postedTweet.getCoordinates().getLatitude(), 0.1);
-    Assert.assertTrue(hashtag.contains(postedTweet.getEntities().getHashtags()[0].getText()));
+    checkTweet(postedTweet);
+    assertEquals(text, postedTweet.getText());
 
     logger.info("Testing find...");
     Tweet lastTweet = dao.findById(postedTweet.getIdString());
-
-    Assert.assertEquals(postedTweet.getId(), lastTweet.getId());
-    Assert.assertEquals(text, lastTweet.getText());
-    Assert.assertNotNull(lastTweet.getCoordinates());
-    Assert.assertEquals(lon, lastTweet.getCoordinates().getLongitude(), 0.1);
-    Assert.assertEquals(lat, lastTweet.getCoordinates().getLatitude(), 0.1);
-    Assert.assertTrue(hashtag.contains(lastTweet.getEntities().getHashtags()[0].getText()));
+    checkTweet(lastTweet);
+    assertEquals(postedTweet.getId(), lastTweet.getId());
 
     logger.info("Testing delete...");
     Tweet removedTweet = dao.deleteById(lastTweet.getIdString());
+    checkTweet(lastTweet);
+    assertEquals(postedTweet.getId(), removedTweet.getId());
 
-    Assert.assertEquals(postedTweet.getId(), removedTweet.getId());
-    Assert.assertEquals(text, removedTweet.getText());
-    Assert.assertNotNull(removedTweet.getCoordinates());
-    Assert.assertEquals(lon, removedTweet.getCoordinates().getLongitude(), 0.1);
-    Assert.assertEquals(lat, removedTweet.getCoordinates().getLatitude(), 0.1);
-    Assert.assertTrue(hashtag.contains(removedTweet.getEntities().getHashtags()[0].getText()));
-
-    Tweet noTweet = null;
     try {
-      noTweet = dao.findById(removedTweet.getIdString());
+      dao.findById(removedTweet.getIdString());
+      fail();
     } catch (RuntimeException e) {
       logger.info("Content should be deleted, expect 404");
       logger.info(e.getMessage());
     }
-    Assert.assertNull(noTweet);
+  }
+
+  private void checkTweet(Tweet tweet) {
+    assertNotNull(tweet.getText());
+    assertNotNull(tweet.getCoordinates());
+    assertEquals(2, tweet.getCoordinates().getCoordinatesArray().length);
+    assertEquals(LONGITUDE, tweet.getCoordinates().getLongitude(), 0.1);
+    assertEquals(LATITUDE, tweet.getCoordinates().getLatitude(), 0.1);
+    assertTrue(HASHTAG.contains(tweet.getEntities().getHashtags()[0].getText()));
   }
 }
