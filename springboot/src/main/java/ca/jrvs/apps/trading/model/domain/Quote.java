8c005b67283cb846961be3b3cf254c9015c7be53
wxlxy95 <liuxinyu9090@gmail.com>
2020-04-27 23:30:20 -0400

    add Quote DTO skeleton

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/model/domain/Quote.java
@@ -0,0 +1,14 @@
+package ca.jrvs.apps.trading.model.domain;
+
+public class Quote implements Entity<String> {
+
+  @Override
+  public String getID() {
+    return null;
+  }
+
+  @Override
+  public void setId(String s) {
+
+  }
+}
