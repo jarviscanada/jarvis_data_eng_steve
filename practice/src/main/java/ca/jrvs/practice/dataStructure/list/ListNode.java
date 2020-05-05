3ca4615686c38d85d9a96312bee5527bede3bb12
wxlxy95 <liuxinyu9090@gmail.com>
2020-05-05 12:34:22 -0400

    LinkedJList implements JList

---
+++ practice/src/main/java/ca/jrvs/practice/dataStructure/list/ListNode.java
@@ -0,0 +1,36 @@
+package ca.jrvs.practice.dataStructure.list;
+
+public class ListNode<E> {
+
+  E val;
+  ListNode<E> next;
+
+  ListNode() {
+  }
+
+  ListNode(E val) {
+    this.val = val;
+  }
+
+  ListNode(E val, ListNode<E> next) {
+    this.val = val;
+    this.next = next;
+  }
+
+  public E getVal() {
+    return val;
+  }
+
+  public void setVal(E val) {
+    this.val = val;
+  }
+
+  public ListNode<E> getNext() {
+    return next;
+  }
+
+  public void setNext(ListNode<E> next) {
+    this.next = next;
+  }
+
+}
