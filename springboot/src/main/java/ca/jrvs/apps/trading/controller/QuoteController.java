975c0c90ff5ace5c6c203d43b6c69353a5f3fbd8
wxlxy95 <liuxinyu9090@gmail.com>
2020-05-08 21:47:38 -0400

    implement QuoteController

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
@@ -1,12 +1,17 @@
 package ca.jrvs.apps.trading.controller;
 
 import ca.jrvs.apps.trading.model.domain.IexQuote;
+import ca.jrvs.apps.trading.model.domain.Quote;
 import ca.jrvs.apps.trading.service.QuoteService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -33,4 +38,47 @@ public class QuoteController {
     }
   }
 
+  @PutMapping(path = "/iexMarketData")
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public List<Quote> updateMarketData() {
+    try {
+      return service.updateMarketData();
+    } catch (Exception e) {
+      throw ResponseExceptionUtil.getResponseStatusException(e);
+    }
+  }
+
+  @PutMapping(path = "/")
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public Quote putQuote(@RequestBody Quote quote) {
+    try {
+      return service.saveQuote(quote);
+    } catch (Exception e) {
+      throw ResponseExceptionUtil.getResponseStatusException(e);
+    }
+  }
+
+  @PostMapping(path = "/tickerId/{tickerId}")
+  @ResponseStatus(HttpStatus.CREATED)
+  @ResponseBody
+  public Quote createQuote(String tickerId) {
+    try {
+      return service.saveQuote(tickerId);
+    } catch (Exception e) {
+      throw ResponseExceptionUtil.getResponseStatusException(e);
+    }
+  }
+
+  @GetMapping(path = "/dailyList")
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public List<Quote> getDailyList() {
+    try {
+      return service.findAllQuotes();
+    } catch (Exception e) {
+      throw ResponseExceptionUtil.getResponseStatusException(e);
+    }
+  }
 }
