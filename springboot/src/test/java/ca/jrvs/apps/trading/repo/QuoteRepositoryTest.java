c557271e5c8ea310106d382b10e7c0507005f7f8
wxlxy95 <liuxinyu9090@gmail.com>
2020-05-13 00:31:56 -0400

    apply DRY to repo test

--- springboot/src/test/java/ca/jrvs/apps/trading/repo/QuoteRepositoryTest.java
+++ springboot/src/test/java/ca/jrvs/apps/trading/repo/QuoteRepositoryTest.java
@@ -1,98 +1,37 @@
 package ca.jrvs.apps.trading.repo;
 
-import static ca.jrvs.apps.trading.TestUtil.NOT_ID;
+import static ca.jrvs.apps.trading.TestUtil.NOT_TICKER;
 import static ca.jrvs.apps.trading.TestUtil.getQuoteRbc;
 import static ca.jrvs.apps.trading.TestUtil.getQuoteShop;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-import ca.jrvs.apps.trading.TestConfig;
 import ca.jrvs.apps.trading.model.domain.Quote;
-import java.util.List;
-import java.util.Optional;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = {TestConfig.class})
-@Sql({"classpath:schema.sql"})
-public class QuoteRepositoryTest {
+public class QuoteRepositoryTest extends BaseRepositoryTest<QuoteRepository, Quote, String> {
 
-  @Autowired
-  private QuoteRepository repository;
-
-  private final static Quote savedQuote1 = getQuoteShop();
-  private final static Quote savedQuote2 = getQuoteRbc();
-
-  @Before
-  public void setUp() {
-    repository.save(savedQuote1);
-    repository.save(savedQuote2);
+  @Override
+  Object modifyOneField(Quote quote) {
+    Integer newAskSize = quote.getAskSize() + 1;
+    quote.setAskSize(newAskSize);
+    return newAskSize;
   }
 
-  @After
-  public void tearDown() {
-    repository.deleteAll();
+  @Override
+  Object getOneField(Quote quote) {
+    return quote.getAskSize();
   }
 
-  @Test
-  public void find() {
-    List<Quote> all = repository.findAll();
-    assertEquals(2, all.size());
-
-    Optional<Quote> quote1 = repository.findById(savedQuote1.getId());
-    assertTrue(quote1.isPresent());
-    assertEquals(savedQuote1, quote1.get());
-
-    Optional<Quote> quote2 = repository.findById(savedQuote2.getId());
-    assertTrue(quote2.isPresent());
-    assertEquals(savedQuote2, quote2.get());
-
-    Optional<Quote> quote3 = repository.findById(NOT_ID);
-    assertTrue(quote3.isEmpty());
+  @Override
+  Quote getTestEntity1() {
+    return getQuoteShop();
   }
 
-  @Test
-  public void delete() {
-    assertEquals(2, repository.count());
-    assertTrue(repository.existsById(savedQuote1.getId()));
-    assertTrue(repository.existsById(savedQuote2.getId()));
-
-    repository.deleteById(savedQuote1.getId());
-    assertEquals(1, repository.count());
-    assertFalse(repository.existsById(savedQuote1.getId()));
-    assertTrue(repository.existsById(savedQuote2.getId()));
-
-    repository.delete(savedQuote2);
-    assertEquals(0, repository.count());
-    assertFalse(repository.existsById(savedQuote2.getId()));
-
-    // do nothing
-    repository.delete(savedQuote1);
-    repository.delete(savedQuote2);
+  @Override
+  Quote getTestEntity2() {
+    return getQuoteRbc();
   }
 
-  @Test
-  public void update() {
-    List<Quote> all = repository.findAll();
-    int newAskSize = all.get(0).getAskSize() + 1;
-    all.get(0).setAskSize(newAskSize);
-    repository.saveAll(all);
-
-    Optional<Quote> quote1 = repository.findById(all.get(0).getId());
-    assertTrue(quote1.isPresent());
-    assertEquals(newAskSize, quote1.get().getAskSize().intValue());
-
-    Optional<Quote> quote2 = repository.findById(all.get(1).getId());
-    assertTrue(quote2.isPresent());
-    assertEquals(savedQuote2, quote2.get());
+  @Override
+  String getNonExistId() {
+    return NOT_TICKER;
   }
-
 }
---
+++ springboot/src/test/java/ca/jrvs/apps/trading/repo/TraderRepositoryTest.java
@@ -0,0 +1,36 @@
+package ca.jrvs.apps.trading.repo;
+
+import static ca.jrvs.apps.trading.TestUtil.getTraderDavid;
+import static ca.jrvs.apps.trading.TestUtil.getTraderYasuo;
+
+import ca.jrvs.apps.trading.model.domain.Trader;
+
+public class TraderRepositoryTest extends BaseRepositoryTest<TraderRepository, Trader, Integer> {
+
+  @Override
+  Trader getTestEntity1() {
+    return getTraderDavid();
+  }
+
+  @Override
+  Trader getTestEntity2() {
+    return getTraderYasuo();
+  }
+
+  @Override
+  Integer getNonExistId() {
+    return 10;
+  }
+
+  @Override
+  Object modifyOneField(Trader trader) {
+    final String newName = "Newman";
+    trader.setFirstName(newName);
+    return newName;
+  }
+
+  @Override
+  Object getOneField(Trader trader) {
+    return trader.getFirstName();
+  }
+}
