01f6c2b8867a31dce16682463f15a9fdbb755726
wxlxy95 <liuxinyu9090@gmail.com>
2020-05-14 13:18:02 -0400

    PositionRepository uses PositionId

--- springboot/src/main/java/ca/jrvs/apps/trading/repo/PositionRepository.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/repo/PositionRepository.java
@@ -1,10 +1,16 @@
 package ca.jrvs.apps.trading.repo;
 
+import ca.jrvs.apps.trading.model.domain.Id.PositionId;
 import ca.jrvs.apps.trading.model.domain.Position;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PositionRepository extends JpaRepository<Position, Integer> {
+public interface PositionRepository extends JpaRepository<Position, PositionId> {
+
+  List<Position> findByAccountId(Integer accountId);
+
+  List<Position> findByTicker(String ticker);
 
 }
