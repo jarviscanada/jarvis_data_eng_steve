f9395372c24da1d57b11632becc7a528097a9371
wxlxy95 <liuxinyu9090@gmail.com>
2020-05-14 13:17:14 -0400

    Position uses PositionId

--- springboot/src/main/java/ca/jrvs/apps/trading/model/domain/Position.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/model/domain/Position.java
@@ -1,22 +1,60 @@
 package ca.jrvs.apps.trading.model.domain;
 
+import ca.jrvs.apps.trading.model.domain.Id.PositionId;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import org.hibernate.annotations.Immutable;
 
 @javax.persistence.Entity
 @Immutable
+@IdClass(PositionId.class)
 public class Position implements Entity<Integer> {
 
   @Id
   @Column(name = "account_id")
+  @NotNull
   private Integer accountId;
 
+  @Id
   @Column
+  @NotBlank
   private String ticker;
 
   @Column
+  @NotNull
   private Integer position;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Position position1 = (Position) o;
+    return accountId.equals(position1.accountId) &&
+        ticker.equals(position1.ticker) &&
+        position.equals(position1.position);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(accountId, ticker, position);
+  }
+
+  @Override
+  public String toString() {
+    return "Position{" +
+        "accountId=" + accountId +
+        ", ticker='" + ticker + '\'' +
+        ", position=" + position +
+        '}';
+  }
 
   @Override
   public Integer getId() {
