3b498de100ac865fd772af05ca61626caaeeba17
wxlxy95 <liuxinyu9090@gmail.com>
2020-06-04 19:38:23 -0400

    configure dataSource for RDS

--- springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
@@ -29,7 +29,7 @@ public class AppConfig {
   }
 
   @Bean
-  public PoolingHttpClientConnectionManager poolingHttpClientConnectionManager(){
+  public PoolingHttpClientConnectionManager poolingHttpClientConnectionManager() {
     PoolingHttpClientConnectionManager manager = new PoolingHttpClientConnectionManager();
     manager.setMaxTotal(50);
     manager.setDefaultMaxPerRoute(50);
@@ -38,9 +38,23 @@ public class AppConfig {
 
   @Bean
   public DataSource dataSource() {
-    String url = System.getenv("PSQL_URL");
-    String user = System.getenv("PSQL_USER");
-    String password = System.getenv("PSQL_PASSWORD");
+    String url;
+    String user;
+    String password;
+
+    if (System.getenv("RDS_HOSTNAME") != null) {
+      url = "jdbc:postgresql://"
+          + System.getenv("RDS_HOSTNAME")
+          + ":" + System.getenv("RDS_PORT")
+          + "/"
+          + "";
+      user = System.getenv("RDS_USERNAME");
+      password = System.getenv("RDS_PASSWORD");
+    } else {
+      url = System.getenv("PSQL_URL");
+      user = System.getenv("PSQL_USER");
+      password = System.getenv("PSQL_PASSWORD");
+    }
 
     BasicDataSource basicDataSource = new BasicDataSource();
     basicDataSource.setUrl(url);
