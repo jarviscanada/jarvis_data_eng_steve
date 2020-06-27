bc6df9e31e1cf741768635aa66839a01e6bf31c7
wxlxy95 <liuxinyu9090@gmail.com>
2020-06-27 01:01:12 -0400

    complete hasCycle and removeNthFromEnd

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/LinkedListUtil.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/LinkedListUtil.java
@@ -13,4 +13,38 @@ public class LinkedListUtil<E> {
     return slow;
   }
 
+  public boolean hasCycle(ListNode<E> head) {
+    ListNode<E> slow = head, fast = head;
+    while (fast != null && fast.getNext() != null) {
+      slow = slow.getNext();
+      fast = fast.getNext().getNext();
+      if (slow == fast) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public ListNode<E> removeNthFromEnd(ListNode<E> head, int n) {
+    ListNode<E> dummy = new ListNode<>();
+    dummy.setNext(head);
+    ListNode<E> first = dummy, second = dummy;
+    for (int i = 0; i <= n; i++) {
+      if (first == null) {
+        // n greater than size of list!
+        return head;
+      }
+      first = first.getNext();
+
+    }
+    while (first != null) {
+      first = first.getNext();
+      second = second.getNext();
+    }
+    if (second.getNext() != null) {
+      second.setNext(second.getNext().getNext());
+    }
+    return dummy.getNext();
+  }
+
 }
